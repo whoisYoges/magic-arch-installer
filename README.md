d3f5aa45259427cf43f22b3c0ffa2d712131b7ab
YogeshLamichhane <contact.yogesh@protonmail.com>
2021-10-11 08:27:18 +0545

    install arch in few clicks

---
+++ README.md
@@ -0,0 +1,17 @@
+# magic-arch-installer
+Install arch linux just in few clicks. Even a 5 years kid can install arch now.
+
+### What is magic-arch-installer?
+magic-arch-installer is a bash script with the commands required to install arch linux listed in order that makes a person install arch in easier faster and more convenient way.
+
+## ⚠️ Warning: Currently it supports uefi installation only. Legacy installation type will be added later if required.
+
+### Usage
+1. boot your arch installation media in your machine and connect to the internet
+
+2. get arch-install.sh in your machine make it executable and run it.
+```
+curl https://raw.githubusercontent.com/YogeshLamichhane/magic-arch-installer/master/install.sh > install.sh
+chmod +x install.sh
+./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,99 @@
+echo "Welcome to magic-arch-installer."
+echo "Install arch linux in few clicks. Even a 5 years kid can install arch now."
+echo "You need to be connected to internet before running this script. If you are not connected and if you have ethernet, plug it and connect to the internet, else see arch wiki how to connect wifi (https://wiki.archlinux.org/title/Iwd#iwctl)"
+echo "checking internet connection"
+ping -c 3 yogeshlamichhane.com.np
+sleep 2s
+clear
+pacman -Sy
+clear
+timedatectl set-ntp true
+lsblk
+echo "Enter the drive to create partitions for linux systems ( eg: /dev/sda). "
+echo "root, home, swap, boot/efi partitions need to be created. home and swap partitions are optional but recommended: "
+read drive
+
+# you can use cfdisk or gdisk in place of fdisk as well.
+fdisk $drive
+lsblk
+echo "Enter the root partition (eg: /dev/sda1): "
+read rootpartition
+mkfs.ext4 $rootpartition
+mount $rootpartition /mnt
+clear
+lsblk
+read -p "Did you also create separate home partition? [y/n]: " answerhome
+if [[ $answerhome = y ]] ; then
+  echo "Enter home partition: "
+  read homepartition
+  mkfs.ext4 $homepartition
+  mkdir /mnt/home
+  mount $homepartition /mnt/home
+fi
+clear
+lsblk
+read -p "Did you also create swap partition? [y/n]: " answerswap
+if [[ $answerswap = y ]] ; then
+  echo "Enter swap partition: "
+  read swappartition
+  mkswap $swappartition
+  swapon $swappartition
+fi
+clear
+lsblk
+echo "Enter EFI partition: "
+read efipartition
+mkfs.vfat -F 32 $efipartition
+mkdir -p /mnt/boot/efi
+mount $efipartition /mnt/boot/efi
+clear
+lsblk
+sleep 2s
+#Replace kernel header file and with your requirements (eg linux-zen linux-zen-headers)
+#replace intel-ucode with amd-ucode if you use amd processor
+pacstrap /mnt base base-devel linux-lts linux-lts-headers intel-ucode
+genfstab -U /mnt >> /mnt/etc/fstab
+sed '1,/^#part2$/d' install.sh > /mnt/post_install.sh
+chmod +x /mnt/post_install.sh
+arch-chroot /mnt ./post_install.sh
+exit
+
+#part2
+#Replace Asia/Kathmandu with your timezone
+clear
+ln -sf /usr/share/zoneinfo/Asia/Kathmandu /etc/localtime
+hwclock --systohc
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "Enter your computer name: "
+read hostname
+echo $hostname > /etc/hostname
+echo "127.0.0.1       localhost" >> /etc/hosts
+echo "::1             localhost" >> /etc/hosts
+echo "127.0.1.1       $hostname" >> /etc/hosts
+echo "Enter password for root user:"
+passwd
+#if you are dualbooting, add os-prober with grub and efibootmgr
+pacman -S grub efibootmgr
+echo "Installing grub bootloader in /boot/efi parttiton"
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --removable
+grub-mkconfig -o /boot/grub/grub.cfg
+pacman -S networkmanager
+systemctl enable NetworkManager
+clear
+echo "Enter username to add a user: "
+read username
+useradd -m -g users -G wheel -s /bin/bash $username
+echo "Enter password for the username you put just now:"
+passwd $username
+echo "NOTE: ALWAYS REMEMBER THIS USERNAME AND PASSWORD YOU PUT JUST NOW."
+sleep 2s
+#Adding sudo previliages to the user you created
+echo "$username ALL=(ALL) ALL" >> /etc/sudoers.d/$username
+clear
+echo "Pre-Installation Finished."
+echo "You'll exit to installation media after 5 seconds. Then after Unmount all the file systems with 'umount -R /mnt' and then Reboot with 'reboot' commands and then login with the username and password you put earlier."
+sleep 5s
+rm /post_install.sh
+exit
