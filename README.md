0a0d09f3d1a274674459677ae2fe2c36962776da
YogeshLamichhane <contact.yogesh@protonmail.com>
2022-01-06 19:39:48 +0545

    added legacy installation support

--- README.md
+++ README.md
@@ -4,8 +4,6 @@ Install arch linux just in few clicks. Even a 5 years kid can install arch now.
 ### What is magic-arch-installer?
 magic-arch-installer is a bash script with the commands required to install arch linux listed in order that makes a person install arch in much easier, faster and more convenient way.
 
-## ⚠️ Warning: Currently it supports uefi installation only. Legacy installation type will be added later if required.
-
 ### Usage
 1. boot your arch installation media in your machine and connect to the internet.
 
@@ -15,17 +13,33 @@ For [Wifi connection check here](https://wiki.archlinux.org/title/Iwd#iwctl).
 
 For [mobile broadband connection check here](https://wiki.archlinux.org/title/Mobile_broadband_modem#ModemManager).
 
-2. get base-install.sh in your installation media.
+### For UEFI Installation Type
+2 a. get base-install.sh in your installation media.
 ```
 curl https://raw.githubusercontent.com/whoisYoges/magic-arch-installer/master/base-install.sh > base-install.sh
 ```
-3. open the script with text editor vim or nano and make necessary changes if required (such as timezone, kernel and kernel-headers, filesystem type, etc) and save it.
+3 a. open the script with text editor vim or nano and make necessary changes if required (such as timezone, kernel and kernel-headers, filesystem type, etc) and save it.
 
-4. make it executable.
+4 a. make it executable.
 ```
 chmod +x base-install.sh
 ```
-5. run it.
+5 a. run it.
 ```
 ./base-install.sh
+```
+## For Legacy Installation Type
+2 b. get base-install.sh in your installation media.
+```
+curl https://raw.githubusercontent.com/whoisYoges/magic-arch-installer/master/legacy-base-install.sh > legacy-base-install.sh
+```
+3 b. open the script with text editor vim or nano and make necessary changes if required (such as timezone, kernel and kernel-headers, filesystem type, etc) and save it.
+
+4 b. make it executable.
+```
+chmod +x legacy-base-install.sh
+```
+5 b. run it.
+```
+./legacy-base-install.sh
 ```
---
+++ legacy-base-install.sh
@@ -0,0 +1,102 @@
+echo "Welcome to magic-arch-installer."
+echo "Install arch linux in few clicks. Even a 5 year kid can install arch now."
+pacman -Sy
+timedatectl set-ntp true
+clear
+lsblk
+echo "Enter the drive to create partitions for linux systems ( eg: /dev/sda). "
+echo "root, home, swap partitions need to be created. home and swap partitions are optional but recommended: "
+read drive
+
+# you can use fdisk or gdisk in place of fdisk as well.
+cfdisk $drive
+clear
+lsblk
+echo "Enter the root partition (eg: /dev/sda1): "
+read rootpartition
+mkfs.ext4 $rootpartition
+mount $rootpartition /mnt
+clear
+lsblk
+read -p "Did you also create separate home partition? [y/n]: " answerhome
+if [[ $answerhome = y ]] ; then
+  echo "Enter home partition (eg: /dev/sda2): "
+  read homepartition
+  mkfs.ext4 $homepartition
+  mkdir /mnt/home
+  mount $homepartition /mnt/home
+fi
+clear
+lsblk
+read -p "Did you also create swap partition? [y/n]: " answerswap
+if [[ $answerswap = y ]] ; then
+  echo "Enter swap partition (eg: /dev/sda3): "
+  read swappartition
+  mkswap $swappartition
+  swapon $swappartition
+fi
+clear
+lsblk
+sleep 5s
+#Replace kernel header file and with your requirements (eg linux-zen linux-zen-headers)
+#replace intel-ucode with amd-ucode if you use amd processor
+pacstrap /mnt base base-devel linux-lts linux-lts-headers intel-ucode
+genfstab -U /mnt >> /mnt/etc/fstab
+sed '1,/^#part2$/d' legacy-base-install.sh > /mnt/post_base-install.sh
+chmod +x /mnt/post_base-install.sh
+arch-chroot /mnt ./post_base-install.sh
+clear
+umount -R /mnt
+echo "Pre-Installation Finished. Rebooting in 10 seconds"
+echo "You can install gui and other utilities after rebooting."
+echo "You can check https://github.com/whoisYoges/magic-arch-installer/ for gui and other utilities installation/automation scripts."
+sleep 10s
+reboot
+exit
+
+#part2
+#Replace Asia/Kathmandu with your timezone
+clear
+ln -sf /usr/share/zoneinfo/Asia/Kathmandu /etc/localtime
+hwclock --systohc
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+clear
+echo "Enter your computer name: "
+read hostname
+echo $hostname > /etc/hostname
+echo "127.0.0.1       localhost" >> /etc/hosts
+echo "::1             localhost" >> /etc/hosts
+echo "127.0.1.1       $hostname" >> /etc/hosts
+clear
+#if you are dualbooting, add os-prober with grub and efibootmgr
+
+echo"Installing Grub"
+sleep 5s
+echo "Getting grub bootloader."
+pacman -Sy --needed grub 
+echo "Enter the drive to install grub ( eg: /dev/sda). "
+echo "Note: Remember not to add sda1 or sda2 it's just /dev/sda or /dev/sdb."
+read grubdrive
+grub-install --target=i386-pc $grubdrive
+grub-mkconfig -o /boot/grub/grub.cfg
+pacman -S networkmanager
+systemctl enable NetworkManager
+clear
+echo "Add password for root user:"
+passwd
+clear
+echo "Add a regular user (Set Username): "
+read username
+useradd -m -g users -G wheel -s /bin/bash $username
+echo "Enter password for the user you created just now:"
+passwd $username
+clear
+echo "NOTE: ALWAYS REMEMBER THIS USERNAME AND PASSWORD YOU PUT JUST NOW."
+sleep 3s
+#Adding sudo previliages to the user you created
+echo "$username ALL=(ALL) ALL" >> /etc/sudoers.d/$username
+clear
+rm /post_base-install.sh
+exit
